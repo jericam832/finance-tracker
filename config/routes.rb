1cf2e8db63050334c062d4b0d85adf75b5d38cfd
Jerica Mitchell <jericam32@gmail.com>
2018-02-01 02:03:24 +0000

    Added friendhip functionality, finishing touches to project

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,17 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   
-  root 'users#my_portfolio'
+  root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'index', to: 'welcome#index'
+  # get 'index', to: 'welcome#index'
   get 'search_stocks', to: 'stocks#search'
-  
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: 'users#add_friend'
+  
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   
 
 
